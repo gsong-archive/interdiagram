ab7633a374cf7af1f4fc97add0ecde4353cdfc6c
George Song <george@monozuku.com>
2016-11-26 09:03:23 -0800

    Initial project layout

---
+++ Makefile
@@ -0,0 +1,20 @@
+.DEFAULT_GOAL := test
+
+LOCAL_IN = requirements/local.in
+LOCAL_TXT = requirements/local.txt
+
+
+install-dev-requirements:
+	## Install requirements for local development environment
+	pip install -q pip-tools
+	pip-sync requirements/*.txt
+	pip install -e .
+
+
+pip-compile:
+	## Update requirements/*.txt with latest packages from requirements/*.in
+	pip install -q pip-tools
+	pip-compile -U requirements/app.in
+ifneq (, $(wildcard $(LOCAL_IN)))
+	pip-compile -U $(LOCAL_IN)
+endif
---
+++ setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+import re
+
+from setuptools import setup
+
+
+def get_content(*fps):
+    fp = Path(__file__).parent.joinpath(*fps)
+    content = ''
+    if fp.exists():
+        with fp.open() as f:
+            content = f.read()
+    return content
+
+
+def get_requirements():
+    reqs = get_content('requirements/app.in').splitlines()
+    return reqs
+
+
+def get_version(*file_paths):
+    """
+    Extract the version string from the file at the given path fragments.
+    """
+    version_file = get_content(*file_paths)
+    version_match = re.search(
+        r"""^__version__ = ['"]([^'"]*)['"]""", version_file, re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+VERSION = get_version('interdiagram', '__init__.py')
+README = get_content('README.md')
+CHANGELOG = get_content('CHANGELOG.md')
+
+setup(
+    name='interdiagram',
+    version=VERSION,
+    description='Generate interaction diagrams',
+    long_description=README + '\n\n' + CHANGELOG,
+    url='https://github.com/gsong/interdiagram',
+    author='George Song',
+    author_email='george@damacy.net',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='interaction diagram sitemap',
+
+    packages=['interdiagram'],
+    install_requires=get_requirements(),
+
+    entry_points={
+        'console_scripts': [
+            'interdiagram=interdiagram.bin.interdiagram:cli',
+        ],
+    },
+)
