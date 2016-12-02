e611e9518945fa38165e8adf7103561f438b70b1
George Song <george@monozuku.com>
2016-12-01 16:56:17 -0800

    Add subcommand to process directory
    Close #7

--- interdiagram/bin/interdiagram.py
+++ interdiagram/bin/interdiagram.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import List
+from pathlib import Path
+from typing import Iterable, List, TypeVar
+from typing.io import IO
 
 import click
 import yaml
@@ -10,16 +12,59 @@ from ..models import Diagram
 
 click.disable_unicode_literals_warning = True
 
+FileType = TypeVar('FileType', IO, Path)
 
-# TODO: Correct documentation schema once it's frozen
-@click.command()
-@click.argument('yaml-file', nargs=-1, type=click.File())
-@click.argument('output-file', type=click.Path(resolve_path=True))
-def cli(
-        yaml_file: List,
+
+def _is_file_obj(
+        f: FileType
+) -> bool:
+    read_attr = getattr(f, 'read', None)
+    has_read_method = callable(read_attr)
+    return has_read_method
+
+
+def _draw_files(
+        files: Iterable[FileType],
         output_file: str
 ) -> None:
-    """Generate interaction/sitemap diagram.
+    diagram = Diagram()
+    for f in files:
+        # TODO: Validate against schema
+        if not _is_file_obj(f):
+            f = f.open()  # type: ignore
+        diagram.process_spec(yaml.load(f))
+    diagram.draw(output_file)
+
+
+# TODO: Correct documentation schema once it's frozen
+@click.group()
+def cli():
+    """Generate interaction/sitemap diagram."""
+
+
+@cli.command('dir')
+@click.argument(
+    'directory',
+    type=click.Path(exists=True, file_okay=False, resolve_path=True)
+)
+@click.argument('output-file', type=click.Path(resolve_path=True))
+def directory(
+        directory: str,
+        output_file: str
+) -> None:
+    """Specify a directory where YAML files reside."""
+    files = Path(directory).glob('**/*.y*ml')
+    _draw_files(files, output_file)
+
+
+@cli.command()
+@click.argument('yaml-file', nargs=-1, type=click.File())
+@click.argument('output-file', type=click.Path(resolve_path=True))
+def files(
+        yaml_file: List[IO],
+        output_file: str
+) -> None:
+    """Specify individual YAML files.
 
     Example: interdiagram data1.yaml data2.yaml output.pdf
 
@@ -43,11 +88,7 @@ def cli(
           - Go to detail:
           - Add to registry:
     """
-    diagram = Diagram()
-    for f in yaml_file:
-        # TODO: Validate against schema
-        diagram.process_spec(yaml.load(f))
-    diagram.draw(output_file)
+    _draw_files(yaml_file, output_file)
 
 
 if __name__ == '__main__':
